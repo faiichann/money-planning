3661b937678a8cac5d0aef47c26215a2692a71ba
faiichann <fainichkamon@gmail.com>
2026-03-30 15:42:47 +0700

    fix: wording

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -22,7 +22,7 @@ const Home = () => {
             className="w-full h-[60px] py-2 bg-white text-green-900 rounded-lg shadow hover:bg-green-900 hover:text-white"
             onClick={() => navigate("/retirement")}
           >
-            วางแผนเงินเกษียณ
+            วางแผนเงินเพื่อการเกษียณ
           </button>
 
           <button
